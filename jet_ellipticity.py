53c25081cd029a16cd0cef80f9cfdd80f0ffde45
Girish Kulkarni <kulkarni@ast.cam.ac.uk>
2015-07-21 12:28:09 +0100

    Clean up

--- jet_ellipticity.py
+++ jet_ellipticity.py
@@ -1,3 +1,12 @@
+"""
+
+File: jet_ellipticity.py
+
+Jet morphology from a BH binary on an elliptical orbit that is in the
+GW-dominated phase of its inspiral (Figures 4).
+
+"""
+
 import matplotlib as mpl
 mpl.rcParams['text.usetex'] = True 
 mpl.rcParams['font.family'] = 'serif'
@@ -8,20 +17,19 @@ import numpy as np
 import math 
 import sys
 
-#psi = 20.0 # degrees
 i = 40.0 # degrees
 beta = 0.90 # jet speed in units of c 
 d = 100.0 # Mpc; Distance between jet and observer
+psi0 = 2.0*np.pi/180.0 # radians 
 
 m1 = 0.5e10 #msun
 m2 = m1 #msun
 mu = m1*m2/(m1+m2) # reduced mass
 zeta = 4.0*mu/(m1+m2) # See Loeb 2010 near eqn.1
-print 'zeta=', zeta
 
 e = 0.6 # orbital eccentricity 
 def F(e):
-    # See Merritt and Milos 2005 eqn 7 
+    # Merritt and Milosavljetvic (2005); Eqn. (7). 
     return (1.0-e*e)**3.5 * (1.0 + 73.0*e*e/24.0 + 37.0*e*e*e*e/96.0)
 
 print 'e=', e
@@ -29,7 +37,6 @@ print 'F(e)=', F(e)
 
 M = 1.0e10 # Msun; total mass of the equal-mass binary
 Mdot = 1.0 # Eddington units
-#a0 = 3.241e-3*(M*1.0e-8)**(3./4.) # pc
 a0 = 8.3e-3*(M*1.0e-8)**(3./4.)*(Mdot**(-0.25)) # pc 
 pcto_10to16cm = 0.003241
 a0 /= pcto_10to16cm # 1.0e16 cm
@@ -39,11 +46,9 @@ coeff /= F(e) # eccentricity
 
 d *= 1.0e3 # kpc
 
-#output_filename = 'jet_i%2d_psi%2d_beta%3.2f' % (i, psi, beta)
 output_filename = 'jet_i%2d_beta%3.2f_mdot%3.2f' % (i, beta, Mdot)
 save_pdf = False
 
-#psi *= np.pi/180.0 # radians 
 i *= np.pi/180.0 # radians 
 
 c = 3.0e5 # km/s; speed of light
@@ -60,16 +65,14 @@ def binary_orbital_period(a_16):
     return t
 
 def half_opening_angle_intrinsic(a_16):
-    psi0 = 2.0*np.pi/180.0 # radians 
     angle = np.arcsin(np.sin(psi0)*a0/a_16)
-    return angle #*180.0/np.pi # degrees 
+    return angle 
 
 case = int(sys.argv[1])
 if case == 0: 
     t = np.logspace(-2.0,2.0,10000000)
     output_filename += '_full'
 elif case == 1:
-    #t = np.linspace(10.0,14.0,10000)
     t = np.logspace(-2.0,2.0,10000000)
     t = t[6000000:9500000]
     output_filename += '_zoom1'
@@ -82,22 +85,19 @@ t0 = t[0]
 def t_binary(time):
     t_merge=abs(coeff)*a0**4/4.0
     return np.abs(time-t_merge) # yr
-    #return np.abs(time-64001.477505390176) # yr
 
 def Omega(time):
-    # Angular velocity times time for jet precession 
     period = binary_orbital_period(binary_separation_gw(t_binary(time))) # yr 
     return 2.0*np.pi*time/period # radians
 
 def vel(time):
-    # We are following geometry from Gower et al. Figure 1.
     psi = half_opening_angle_intrinsic(binary_separation_gw(t_binary(time)))
     vx = beta*c*(np.sin(psi)*np.sin(i)*np.cos(Omega(time)) + np.cos(psi)*np.cos(i))
     vy = beta*c*np.sin(psi)*np.sin(Omega(time))
     vz = beta*c*(np.cos(psi)*np.sin(i)-np.sin(psi)*np.cos(i)*np.cos(Omega(time)))
     return sign*vx, sign*vy, sign*vz # km/s
 
-sign = 1
+sign = 1 # forward jet 
 
 velx, vely, velz = vel(t)
 
@@ -110,7 +110,7 @@ z_obs = z/(1.0-velx/c)
 phi_y_obs = y_obs/d * 180.0/np.pi * 3600.0 # arcsec
 phi_z_obs = z_obs/d * 180.0/np.pi * 3600.0 # arcsec
 
-sign = -1
+sign = -1 # backward jet 
 
 velx, vely, velz = vel(t)
 
@@ -130,10 +130,8 @@ phi_z_obs *= 1.0e3 # mas
 
 fig = plt.figure(figsize=(7, 7), dpi=100)
 ax = fig.add_subplot(1, 1, 1)
-#ax.set_ylim(-0.01,0.01)
 if case==1:
     ax.set_ylim(-50.0,50.0)
-    #ax.set_xlim(-0.01,0.01)
 ax.plot(phi_z_obs,phi_y_obs,c='k',lw=1,rasterized=True)
 if case!=1: 
     ax.plot(phi_z_obsb,phi_y_obsb,c='k',lw=1,rasterized=True)
--- jet_unqualmasses.py
+++ jet_unqualmasses.py
@@ -1,3 +1,12 @@
+"""
+
+File: jet_unequalmasses.py
+
+Jet morphology from a BH binary that is in the GW-dominated phase of
+its inspiral (Figures 4).  BH masses are not assumed to be equal.
+
+"""
+
 import matplotlib as mpl
 mpl.rcParams['text.usetex'] = True 
 mpl.rcParams['font.family'] = 'serif'
@@ -7,27 +16,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math 
 import sys
-from matplotlib import cm
-from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-i = 30.0 # degrees
-i *= np.pi/180.0 # radians 
-beta = 0.9 # jet speed in units of c 
-d = 100.0 # Mpc; Angular distance between jet and observer
-d *= 1.0e3 # kpc
-psi0 = 2.0*np.pi/180.0 # radians
+i = 40.0 # degrees
+beta = 0.90 # jet speed in units of c 
+d = 100.0 # Mpc; Distance between jet and observer
+psi0 = 2.0*np.pi/180.0 # radians 
+
+m1 = 0.5e10 #msun
+m2 = m1 #msun
+mu = m1*m2/(m1+m2) # reduced mass
+zeta = 4.0*mu/(m1+m2) # See Loeb 2010 near eqn.1
+print 'zeta=', zeta 
+
 M = 1.0e10 # Msun; total mass of the equal-mass binary
 Mdot = 1.0 # Eddington units
 a0 = 8.3e-3*(M*1.0e-8)**(3./4.)*(Mdot**(-0.25)) # pc 
 pcto_10to16cm = 0.003241
 a0 /= pcto_10to16cm # 1.0e16 cm
-coeff = -2.56e5/(M*1.0e-8)**3 
+coeff = -2.56e5/(M*1.0e-8)**3
+coeff /= zeta # for unequal-mass binary; see Loeb 2010 eqn. 4
 
-output_filename = 'jet_i%2d_beta%3.2f_mdot%3.2f_image' % (i, beta, Mdot)
-save_pdf = True
+d *= 1.0e3 # kpc
 
-# case = 1, 2 zooms into the central region of the jet 
-case = int(sys.argv[1])
+output_filename = 'jet_i%2d_beta%3.2f_mdot%3.2f' % (i, beta, Mdot)
+save_pdf = False
+
+i *= np.pi/180.0 # radians 
 
 c = 3.0e5 # km/s; speed of light
 yrbys = 3.154e7
@@ -46,10 +60,12 @@ def half_opening_angle_intrinsic(a_16):
     angle = np.arcsin(np.sin(psi0)*a0/a_16)
     return angle #*180.0/np.pi # degrees 
 
+case = int(sys.argv[1])
 if case == 0: 
-    t = np.logspace(-1.0,1.0,10000000)
+    t = np.logspace(-2.0,2.0,10000000)
     output_filename += '_full'
 elif case == 1:
+    #t = np.linspace(10.0,14.0,10000)
     t = np.logspace(-2.0,2.0,10000000)
     t = t[6000000:9500000]
     output_filename += '_zoom1'
@@ -62,6 +78,7 @@ t0 = t[0]
 def t_binary(time):
     t_merge=abs(coeff)*a0**4/4.0
     return np.abs(time-t_merge) # yr
+    #return np.abs(time-64001.477505390176) # yr
 
 def Omega(time):
     # Angular velocity times time for jet precession 
@@ -69,16 +86,14 @@ def Omega(time):
     return 2.0*np.pi*time/period # radians
 
 def vel(time):
-    # Geometry of figure 4 of paper.
+    # We are following geometry from Gower et al. Figure 1.
     psi = half_opening_angle_intrinsic(binary_separation_gw(t_binary(time)))
     vx = beta*c*(np.sin(psi)*np.sin(i)*np.cos(Omega(time)) + np.cos(psi)*np.cos(i))
     vy = beta*c*np.sin(psi)*np.sin(Omega(time))
     vz = beta*c*(np.cos(psi)*np.sin(i)-np.sin(psi)*np.cos(i)*np.cos(Omega(time)))
     return sign*vx, sign*vy, sign*vz # km/s
 
-sign = 1 # forward jet
-
-norm_sep = False
+sign = 1 # forward jet 
 
 velx, vely, velz = vel(t)
 
@@ -90,24 +105,6 @@ z_obs = z/(1.0-velx/c)
 
 phi_y_obs = y_obs/d * 180.0/np.pi * 3600.0 # arcsec
 phi_z_obs = z_obs/d * 180.0/np.pi * 3600.0 # arcsec
-
-alpha = 1.0
-delta = 1.0
-t_observed = t/(1.0-velx/c)
-
-gamma = 1.0/np.sqrt(1.0-beta**2)
-doppler = gamma*(1.0-beta)
-
-intensity = doppler**(3.0+alpha-delta) * (t_observed**(-delta))
-intensity = np.array([0.0 if x!=x else x for x in intensity])
-
-if norm_sep: 
-    max_intensity = intensity.max()
-    intensity /= max_intensity
-    intensity = np.array([np.log10(x) if x>0.0 else -2.0 for x in intensity])
-
-phi_y_obs *= 1.0e3 # mas
-phi_z_obs *= 1.0e3 # mas 
 
 sign = -1 # backward jet 
 
@@ -124,51 +121,21 @@ phi_z_obsb = z_obsb/d * 180.0/np.pi * 3600.0 # arcsec
 
 phi_y_obsb *= 1.0e3 # mas
 phi_z_obsb *= 1.0e3 # mas 
-
-alpha = 1.0
-delta = 1.0
-t_observed = t/(1.0-velx/c)
-
-gamma = 1.0/np.sqrt(1.0-beta**2)
-doppler = gamma*(1.0-beta)
-
-intensity_b = doppler**(3.0+alpha-delta) * (t_observed**(-delta))
-intensity_b = np.array([0.0 if x!=x else x for x in intensity_b])
-
-if norm_sep: 
-    max_intensity_b = intensity_b.max()
-    intensity_b /= max_intensity_b
-    intensity_b = np.array([np.log10(x) if x>0.0 else -2.0 for x in intensity_b])
-
-phi_z = np.concatenate([phi_z_obsb, phi_z_obs])
-phi_y = np.concatenate([phi_y_obsb, phi_y_obs])
-
-intensity = np.concatenate([intensity_b, intensity])
-max_intensity = intensity.max()
-intensity /= max_intensity
-intensity = np.array([np.log10(x) if x>0.0 else -2.0 for x in intensity])
+phi_y_obs *= 1.0e3 # mas
+phi_z_obs *= 1.0e3 # mas 
 
 fig = plt.figure(figsize=(7, 7), dpi=100)
 ax = fig.add_subplot(1, 1, 1)
-
 if case==1:
-    ax.set_ylim(-50,50)
-    ax.set_xlim(-10,30)
-
-s=plt.scatter(phi_z,phi_y,s=4,marker='o',edgecolor='none',vmax=0.0,
-              vmin=-2.0,rasterized=True,c=intensity,cmap=cm.RdBu_r)
-
+    ax.set_ylim(-50.0,50.0)
+ax.plot(phi_z_obs,phi_y_obs,c='k',lw=1,rasterized=True)
+if case!=1: 
+    ax.plot(phi_z_obsb,phi_y_obsb,c='k',lw=1,rasterized=True)
 ax.set_xlabel('mas',labelpad=15)
-ax.set_ylabel('mas',labelpad=15)
-
-divider = make_axes_locatable(ax)
-cax = divider.append_axes("right", "5%", pad="3%")
-cb = plt.colorbar(s, cax=cax)
-cb.set_label('logarithmic intensity', labelpad=20)
-cb.solids.set_edgecolor("face")
+ax.set_ylabel('mas')
 
 if save_pdf: 
     plt.savefig(output_filename+'.pdf',bbox_inches='tight')
 
-
+plt.show()
 
